dbca09dd31ee4020146ffd2e65036bb1e24bad02
Savus <justin.fuzzygreenstuff@gmail.com>
2023-06-20 05:31:27 -0600

    Reset project on a new branch to get some much needed practice in rebuilding it.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import { useState } from 'react'
 import './App.css'
-import { CalculatorForm } from './components/CalculatorForm'
 function App() {
   
-  const [userInformation, setUserInformation] = useState({
-    totalDebt:0,
-    interestRate:0
-  });
-
   return (
     <>
-      <CalculatorForm 
-        userInformation={userInformation}
-        handleUserInformation={(userInformation) => {
-          setUserInformation(userInformation);
-        }}
-      />
+     
     </>
   )
 }
